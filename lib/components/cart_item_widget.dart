98c8357e0b84362e14dcc8af8e0c9a5c4d5ecac0
Marhlon Korb <marhlonkorb@hotmail.com>
2022-11-15 20:36:51 -0300

    Criada a listagem dos itens adicionados no carrinho
    Foi adicionado ao CardItemWidget  um Card e dentro desse card foi inserida uma margem, um componente ListTile e dentro dele um CircleAvatar e um padding para adicionar as informações de preço unitário do produto, descrição e quantidade adicionada.

--- lib/components/cart_item_widget.dart
+++ lib/components/cart_item_widget.dart
@@ -1,15 +1,35 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shop/models/cart_item.dart';
 
-class CardItemWidget extends StatelessWidget{
-final CartItem cartItem;
+class CardItemWidget extends StatelessWidget {
+  final CartItem cartItem;
 
-  const CardItemWidget(this.cartItem, {super.key,});
+  const CardItemWidget(
+    this.cartItem, {
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Text(cartItem.name);
+    return Card(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 15,
+        vertical: 4,
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          child: Padding(
+            padding: const EdgeInsets.all(5),
+            child: FittedBox(
+              child: Text('${cartItem.price}'),
+            ),
+          ),
+        ),
+        title: Text(cartItem.name),
+        subtitle: Text('Total: R\$ ${cartItem.price * cartItem.quantity}'),
+        trailing: Text('${cartItem.quantity}x'),
+      ),
+    );
   }
-  
 }
