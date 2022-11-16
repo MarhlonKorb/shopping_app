b4de8f346e997f0ae91b72625e41cc776ba6c637
Marhlon Korb <marhlonkorb@hotmail.com>
2022-11-15 21:33:47 -0300

    Implementacao da remocao dos itens do carrinho
    Foi implementado no componente CardItemWidget o Widget Dismissible que recebe a key do produto e no atributo onDismissed é possível chamar o Provider do carrinho e remove-lo.
    Também foi implementada a interface do botão de exclusão utilizando o deslizar do dismissible.

--- lib/components/cart_item_widget.dart
+++ lib/components/cart_item_widget.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/models/cart_item.dart';
+
+import '../models/cart.dart';
 
 class CardItemWidget extends StatelessWidget {
   final CartItem cartItem;
@@ -11,24 +14,49 @@ class CardItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(
-        horizontal: 15,
-        vertical: 4,
+    // Componente que possibilita "deslizar" um card ou o que estiver dentro dele
+    // para qualquer direção da tela
+    return Dismissible(
+      key: ValueKey(cartItem.id),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        color: Theme.of(context).errorColor,
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20),
+        margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
       ),
-      child: ListTile(
-        leading: CircleAvatar(
-          backgroundColor: Theme.of(context).colorScheme.primary,
-          child: Padding(
-            padding: const EdgeInsets.all(5),
-            child: FittedBox(
-              child: Text('${cartItem.price}'),
+      onDismissed: (_) {
+        Provider.of<Cart>(context, listen: false).removeItem(
+          cartItem.productId,
+        );
+      },
+      child: Card(
+        margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        child: ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            child: Padding(
+              padding: const EdgeInsets.all(5),
+              child: FittedBox(
+                child: Text('${cartItem.price}'),
+              ),
             ),
           ),
+          title: Text(cartItem.name),
+          subtitle: Text('Total: R\$ ${cartItem.price * cartItem.quantity}'),
+          trailing: Text('${cartItem.quantity}x'),
         ),
-        title: Text(cartItem.name),
-        subtitle: Text('Total: R\$ ${cartItem.price * cartItem.quantity}'),
-        trailing: Text('${cartItem.quantity}x'),
       ),
     );
   }
